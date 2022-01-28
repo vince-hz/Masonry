545b7c7fec8bd33692d88ec7aaa01cf6a64439ec
xuyunshi <405029644@qq.com>
2022-01-28 16:52:26 +0800

    Add support for masonry

---
+++ Package.swift
@@ -0,0 +1,22 @@
+// swift-tools-version:5.5
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Masonry",
+    products: [
+        .library(
+            name: "Masonry",
+            targets: ["Masonry"]),
+    ],
+    dependencies: [
+    ],
+    targets: [
+        .target(name: "Masonry",
+                dependencies: [],
+                path: "Masonry",
+                exclude: ["Info.plist"],
+                publicHeadersPath: nil)
+    ]
+)
